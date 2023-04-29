9129d73796710c3c11d26631614cfab8561d97ad
sachetwasti1997 <sachetwasti61@gmail.com>
2023-04-29 08:16:04 +0530

    Updated event-bus

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/posts/:id/comments', async (req, res) => {
     comments.push({id: commentId, content, status: 'pending'});
     commentsByPostId[req.params.id] = comments;
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'COMMENT',
         data: {
             id: commentId,
@@ -54,7 +54,7 @@ app.post('/events', async (req, res) => {
 
         commentToUpdate.status = status;
 
-        await axios.post('http://localhost:4005/events', {
+        await axios.post('http://event-bus-srv:4005/events', {
             type: 'COMMENT_UPDATED',
             data: {
                 id, status, postId, content
